b3a980fe5bc6e229e27754b177c8175053e3451b
ClaudioBuzi001 <buziclaudio@gmail.com>
2022-05-20 12:17:33 +0200

    Implementazione Test Inserisci di Proprietario

--- src/it/prova/gestioneproprietari/test/TestProprietarioAutomobile.java
+++ src/it/prova/gestioneproprietari/test/TestProprietarioAutomobile.java
@@ -1,6 +1,7 @@
 package it.prova.gestioneproprietari.test;
 
 import it.prova.gestioneproprietari.dao.EntityManagerUtil;
+import it.prova.gestioneproprietari.model.Proprietario;
 import it.prova.gestioneproprietari.service.MyServiceFactory;
 import it.prova.gestioneproprietari.service.automobile.AutomobileService;
 import it.prova.gestioneproprietari.service.proprietario.ProprietarioService;
@@ -23,8 +24,11 @@ public class TestProprietarioAutomobile {
 
 			System.out.println("In tabella Auotmobile ci sono: "+ automobileService.listaTutteAutomobili().size()+ " elementi");
 			
+			//test inserissci
 			
-			
+			testInserisciProprietario(proprietarioService);
+			System.out.println(
+					"In tabella Proprietario ci sono: " + proprietarioService.listaTuttiProprietari().size() + " elementi");
 			
 		} catch (Throwable e) {
 			e.printStackTrace();
@@ -33,6 +37,23 @@ public class TestProprietarioAutomobile {
 			// main
 			EntityManagerUtil.shutdown();
 		}
+		
+		
+	}
+	
+	private static void testInserisciProprietario(ProprietarioService proprietarioService) throws Exception {
+		System.out.println(".......testInserisciProprietario inizio.............");
+		// creo nuovo Proprietario
+		Proprietario daAggiungere = new Proprietario("Claudietto", "Buzzione", "bnvuyauH501is", null);
+
+		// salvo
+		proprietarioService.inserisciNuovo(daAggiungere);
+		// da questa riga in poi il record, se correttamente inserito, ha un nuovo id
+		// (NOVITA' RISPETTO AL PASSATO!!!)
+		if (daAggiungere.getId() <1 )
+			throw new RuntimeException("testInserisciProprietario fallito ");
+
+		System.out.println(".......testInserisciProprietario fine: PASSED.............");
 	}
 
 }
