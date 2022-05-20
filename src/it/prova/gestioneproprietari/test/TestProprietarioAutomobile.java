1efe532dc8a550666123f0c0513d06abcfb73f46
ClaudioBuzi001 <buziclaudio@gmail.com>
2022-05-20 12:10:06 +0200

    Implementazione Test E primo test list di Proprietario

--- src/it/prova/gestioneproprietari/test/TestProprietarioAutomobile.java
+++ src/it/prova/gestioneproprietari/test/TestProprietarioAutomobile.java
@@ -1,10 +1,33 @@
 package it.prova.gestioneproprietari.test;
 
+import it.prova.gestioneproprietari.dao.EntityManagerUtil;
+import it.prova.gestioneproprietari.service.MyServiceFactory;
+import it.prova.gestioneproprietari.service.automobile.AutomobileService;
+import it.prova.gestioneproprietari.service.proprietario.ProprietarioService;
+
 public class TestProprietarioAutomobile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Mi creo i due service
+		ProprietarioService proprietarioService = MyServiceFactory.getProprietarioServiceInstance();
+		AutomobileService automobileService = MyServiceFactory.getAutomobileServiceInstance();
 
+		// Partono i test
+		try {
+
+			// ora con il service posso fare tutte le invocazioni che mi servono
+			// TEST Proprietario
+			// Test List
+			System.out.println(
+					"In tabella Proprietario ci sono: " + proprietarioService.listaTuttiProprietari().size() + " elementi");
+
+		} catch (Throwable e) {
+			e.printStackTrace();
+		} finally {
+			// questa è necessaria per chiudere tutte le connessioni quindi rilasciare il
+			// main
+			EntityManagerUtil.shutdown();
+		}
 	}
 
 }
